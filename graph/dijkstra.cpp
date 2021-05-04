ebda79cef11e7076f83db93438a87368f35f8734
Haruki Fujimoto <haruki@HarukinoMac-mini.local>
2021-05-04 22:44:16 +0900

    templates

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,38 @@
+template<class T> struct Dijkstra{
+  #define INF (numeric_limits<T>::max()/2)
+  using P = pair<T,int>;
+  Graph<T> G;
+  vector<T> d;
+  vector<int> prev;
+  Dijkstra(Graph<T>& G):G(G){}
+  void solve(int s){
+    d.assign(G.n,INF);
+    prev.assign(G.n,-1);
+    d[s] = 0;
+    priority_queue<P,vector<P>,greater<P> > que;
+    que.push(P(0,s));
+    while(que.size()){
+      P p = que.top();que.pop();
+      int v = p.second;
+      if(d[v] < p.first) continue;
+      for(int i = 0;i < G[v].size();i++){
+        Edge<T>& e = G[v][i];
+        if(d[e.to] > d[v] + e.cost){
+          d[e.to] = d[v] + e.cost;
+          prev[e.to] = v;
+          que.push(P(d[e.to] , e.to));
+        }
+      }
+    }
+  }
+  bool reachable(int t){
+    return d[t] != INF;
+  }
+  vector<int> get_path(int t){
+    vector<int> path;
+    for(;t != -1;t = prev[t]){ path.push_back(t); }
+    reverse(path.begin(),path.end());
+    return path;
+  }
+  T& operator[](int i){return d[i];}
+};
---
+++ graph/template.cpp
@@ -0,0 +1,14 @@
+template<class T> struct Edge{
+  int from,to,id; T cost;
+  Edge(int from,int to,T cost):from(from),to(to),cost(cost){}
+  Edge(int from,int to,int id,T cost):from(from),to(to),id(id),cost(cost){}
+  Edge(){}
+};
+template<class T> struct Graph{
+  int n;
+  vector<vector<Edge<T>>> g;
+  Graph(int n):n(n){ g.resize(n); }
+  void add_edge(int from,int to){ g[from].emplace_back(from,to,1); }
+  void add_edge(int from,int to,T cost){ g[from].emplace_back(from,to,cost); }
+  vector<Edge<T>>& operator[](int i){ return g[i]; }
+};
